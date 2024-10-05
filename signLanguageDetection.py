ccdab0586534ef8226a587c4bcf5ecd478ba2271
Gowtham-Pentela <gpentela@pnw.edu>
2024-10-05 13:38:29 -0500

    "Testing Model"

--- signLanguageDetection.py
+++ signLanguageDetection.py
@@ -1,299 +1,164 @@
 # %%
-# Import Libraries
-
-# Warning
-import warnings
-warnings.filterwarnings("ignore")
-
-# Main
 import os
-import glob
+import mediapipe as mp
 import cv2
-import numpy as np
-import pandas as pd
-import gc
-import string
-import time
-import random
-import imutils
-from PIL import Image
-from tqdm import tqdm
-tqdm.pandas()
-
-# Visualization
-import matplotlib
 import matplotlib.pyplot as plt
-import plotly
-import plotly.graph_objects as go
-import plotly.express as px
-from plotly.subplots import make_subplots
-from sklearn.manifold import TSNE
-
-# Model
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Dense, Flatten, Dropout
-from keras.models import load_model, Model
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.metrics import accuracy_score
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils 
+mp_drawing_styles = mp.solutions.drawing_styles
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.9)
 
-# %%
-# Configuration
-class CFG:
-    batch_size = 64
-    img_height = 64
-    img_width = 64
-    epochs = 15
-    num_classes = 36
-    img_channels = 1
-    
-def seed_everything(seed: int):
-    random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    np.random.seed(seed)
-    tf.random.set_seed(seed)
-    
-    
-
-# Directory path
-TRAIN_PATH = 'data/ASL_Dataset/Train'
-
-# Labels
-labels = []
-
-# Get the folder names (which represent the labels)
-label_folders = os.listdir(TRAIN_PATH)
-
-# Append the label names from folder names to the labels list
-labels.extend(label_folders)
-
-print(labels)
-
-
-# %%
-# Create Metadata
-list_path = []
-list_labels = []
-for label in labels:
-    label_path = os.path.join(TRAIN_PATH, label, "*")
-    image_files = glob.glob(label_path)
-    
-    sign_label = [label] * len(image_files)
-    
-    list_path.extend(image_files)
-    list_labels.extend(sign_label)
-
-metadata = pd.DataFrame({
-    "image_path": list_path,
-    "label": list_labels
-})
-
-print(metadata)
-
-
-# Split Dataset to Train 0.7, Val 0.15, and Test 0.15
-X_train, X_test, y_train, y_test = train_test_split(
-    metadata["image_path"], metadata["label"], 
-    test_size=0.15, 
-    random_state=2023, 
-    shuffle=True, 
-    stratify=metadata["label"]
-)
-data_train = pd.DataFrame({
-    "image_path": X_train,
-    "label": y_train
-})
-
-X_train, X_val, y_train, y_val = train_test_split(
-    data_train["image_path"], data_train["label"],
-    test_size=0.15/0.70,
-    random_state=2023,
-    shuffle=True,
-    stratify=data_train["label"]
-)
-data_train = pd.DataFrame({
-    "image_path": X_train,
-    "label": y_train
-})
-data_val = pd.DataFrame({
-    "image_path": X_val,
-    "label": y_val
-})
-data_test = pd.DataFrame({
-    "image_path": X_test,
-    "label": y_test
-})
-
-print(data_train)
-print(data_val)
-print(data_test)
-
-# %%
-# Data Augmentation (Just Rescale)
-def data_augmentation():
-    datagen = ImageDataGenerator(
-        samplewise_center=True,
-        samplewise_std_normalization=True,
-        horizontal_flip=False,
-        rotation_range=30,
-        shear_range=0.2,
-        zoom_range=0.2,  # Zoom in and out
-        width_shift_range=0.2,  # Shift images horizontally
-        height_shift_range=0.2,  # Shift images vertically
-        brightness_range=[0.8, 1.2],  # Randomly change brightness
-        fill_mode='nearest'  # Fill in pixels with nearest value
-    )
-    # Training Dataset
-    train_generator = datagen.flow_from_dataframe(
-        data_train,
-        directory="./",
-        x_col="image_path",
-        color_mode='grayscale',
-        y_col="label",
-        class_mode="categorical",
-        batch_size=CFG.batch_size,
-        target_size=(CFG.img_height, CFG.img_width),
-    )
-
-    # Validation Dataset
-    validation_generator = datagen.flow_from_dataframe(
-        data_val,
-        directory="./",
-        x_col="image_path",
-        color_mode='grayscale',
-        y_col="label",
-        class_mode="categorical",
-        batch_size=CFG.batch_size,
-        target_size=(CFG.img_height, CFG.img_width),
-    )
-    
-    # Testing Dataset
-    test_generator = datagen.flow_from_dataframe(
-        data_test,
-        directory="./",
-        x_col="image_path",
-        color_mode='grayscale',
-        y_col="label",
-        class_mode="categorical",
-        batch_size=1,
-        target_size=(CFG.img_height, CFG.img_width),
-        shuffle=False
-    )
-    
-    return train_generator, validation_generator, test_generator
-
-seed_everything(2023)
-train_generator, validation_generator, test_generator = data_augmentation()
 
 # %% [markdown]
-# # Deep Learning Model
+# ## Label Creation
+# 
 
 # %%
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.regularizers import l2
+data_dir = 'data\ASL_Dataset'
+data = []
+labels = []
+for i in sorted(os.listdir(data_dir)):
+    if i == '.DS_Store':
+        pass
+    else:
+        for j in os.listdir(os.path.join(data_dir,i)):
+            data_aux = []
+            img = cv2.imread(os.path.join(data_dir,i,j))
+            img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization
-from tensorflow.keras.regularizers import l2
-
-# Define the improved model
-model = Sequential()
-
-# First Convolutional Block
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1), padding='same'))
-model.add(MaxPooling2D((2, 2)))
-model.add(BatchNormalization())
-
-# Second Convolutional Block
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D((2, 2)))
-model.add(BatchNormalization())
-
-# Third Convolutional Block
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D((2, 2)))
-model.add(BatchNormalization())
-
-# Fourth Convolutional Block
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D((2, 2)))
-model.add(BatchNormalization())
-
-# Fifth Convolutional Block (Optional for more complexity)
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D((2, 2)))
-model.add(BatchNormalization())
-
-# Fully Connected Layers
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))  # Increase dropout to prevent overfitting
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-
-# Output Layer
-model.add(Dense(28, activation='softmax'))
-
-# Model Summary
-model.summary()
-
-
-# Visualize the model architecture
-print(tf.keras.utils.plot_model(model, to_file='custom_cnn.png', show_shapes=True))
-
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Callbacks for early stopping and model checkpointing
-early_stop = EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)
-checkpoint = ModelCheckpoint('custom_cnn_best_weights.keras', save_best_only=True, monitor='val_accuracy', mode='max')
-
-# Train the model
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples // CFG.batch_size,
-    epochs=CFG.epochs,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples // CFG.batch_size,
-    callbacks=[checkpoint]
-)
-
-# Save the trained model
-model.save('cnn_sign_language.h5')
+            results = hands.process(img_rgb)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    for z in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[z].x
+                        y = hand_landmarks.landmark[z].y
+                        data_aux.append(x)
+                        data_aux.append(y)
+                data.append(data_aux)
+                labels.append(i)
+                
+# save data
+f = open('data.pickle', 'wb')
+pickle.dump({'data':data,'labels':labels},f)
+f.close() 
 
 # %%
-model.save('cnn_sign_language.h5')
 
-# %%
-import matplotlib.pyplot as plt
+for i in sorted(os.listdir(data_dir)):
+    if i == '.DS_Store':
+        pass
+    else:
+        for j in os.listdir(os.path.join(data_dir,i))[0:1]:
+            img = cv2.imread(os.path.join(data_dir,i,j))
+            img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-# Plot training & validation accuracy values
-plt.figure(figsize=(12, 4))
+            results = hands.process(img_rgb)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        img_rgb, # img to draw
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style()
 
-plt.subplot(1, 2, 1)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
+                    )
 
-# Plot training & validation loss values
-plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-
+            
+            plt.figure()
+            plt.title(i)
+            plt.imshow(img_rgb)
 plt.show()
+
+# %%
+print("Data shape:", np.array(data).shape)
+print("Data:", data)
+
+
+# %%
+# split data
+X_train, X_test, y_train, y_test = train_test_split(np.array(data), labels, test_size=0.15, random_state=22, shuffle=True)
+
+# model
+model = RandomForestClassifier(random_state=22)
+model.fit(X_train,y_train)
+
+# predict
+pred=model.predict(X_test)
+
+# accruracy
+accuracy_score(y_test,pred)
+# save model
+f = open('model.p', 'wb')
+pickle.dump({'model':model},f)
+f.close() 
+# load model
+model_dict = pickle.load(open('model.p','rb'))
+model = model_dict['model']
+
+# %% [markdown]
+# ## Detection
+
+# %%
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands:
+    while cap.isOpened():
+
+        data_aux=[]
+        x_ = []
+        y_ = []
+
+        ret, frame = cap.read()
+        H, W, _ = frame.shape
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_rgb = cv2.flip(frame_rgb, 1)
+        frame_rgb.flags.writeable = False
+        results = hands.process(frame_rgb)
+        frame_rgb.flags.writeable = True 
+        frame_rgb = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame_rgb, # img to draw
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing.DrawingSpec(color=(28, 255, 3), thickness=5, circle_radius=10),
+                    mp_drawing.DrawingSpec(color=(236, 255, 3), thickness=5, circle_radius=10)
+                )
+
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
+                    x_.append(x)
+                    y_.append(y)
+
+            x1 = int(min(x_) * W)-10
+            y1 = int(min(y_) * H)-10
+
+            x2 = int(max(x_) * W)-10
+            y2 = int(max(y_) * H)-10
+            prediction = model.predict([np.array(data_aux)[0:42]])[0]
+
+            cv2.rectangle(frame_rgb, (x1,y1-10), (x2,y2), (255,99,173), 6)
+            cv2.putText(frame_rgb, prediction, (x1,y1), cv2.FONT_HERSHEY_DUPLEX, 5, (255,0,0), 5, cv2.LINE_AA)
+
+        cv2.imshow('frame',frame_rgb)  
+        # cv2.waitKey(1)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows() 
 
 
